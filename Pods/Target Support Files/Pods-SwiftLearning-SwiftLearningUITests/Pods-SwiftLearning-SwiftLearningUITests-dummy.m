b1df1f57cb9d0fa20cbb79557f5163dfcd333629
tushar_android <tushar.pandey@bold.com>
2021-06-12 00:50:10 +0530

    pod file added

---
+++ Pods/Target Support Files/Pods-SwiftLearning-SwiftLearningUITests/Pods-SwiftLearning-SwiftLearningUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftLearning_SwiftLearningUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftLearning_SwiftLearningUITests
+@end
